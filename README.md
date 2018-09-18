1806b9f44780f06309c6251bc4db4586f46d88b2
ngarawakimani <dancankimani70@gmail.com>
2018-09-18 12:34:20 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Ipay Web Based Integration
+
+This is a simple implementation of Ipay in javascript if you decide to implement it on your web app without going the REST API way.
+
+## Getting Started
+
+Clone the project by running
+
+```
+git clone https://github.com/ngarawakimani/ipay_js.git
+
+```
---
+++ index.js
@@ -0,0 +1,37 @@
+const fields = {
+    "live" : "0",
+    "oid" : "112",
+    "inv" : "112020102292999",
+    "ttl" : "900",
+    "tel" : "256712375678",
+    "eml" : "kajuej@gmailo.com",
+    "vid" : "demo",
+    "curr" : "KES",
+    "p1" : "airtel",
+    "p2" : "020102292999",
+    "p3" : "",
+    "p4" : "900",
+    "cbk" : "http://localhost.com",
+    "cst" : "1",
+    "crl" : "2"
+};
+
+var datastring = "";
+var hashkey = "demo";
+
+for (const key in fields) {
+datastring += fields[key];
+}
+
+const generated_hash = CryptoJS.HmacSHA1(datastring, hashkey);
+
+var form_fields;
+
+for (const key in fields) {        
+form_fields += '<label>'+key+'</label><input name="'+key+'" type="text" value="'+fields[key]+'"><br/>';
+}
+
+form_fields += '<input name="hsh" type="text" value="'+generated_hash+'" ><br/>';
+form_fields += '<button type="submit">&nbsp;Lipa&nbsp;</button>';
+
+document.getElementById("data-form").innerHTML = form_fields;
